d45ddf07219151321f987b3bc5a93f5f7c9a993d
zhaoliang <zhaoliang@coolchuan.net>
2016-01-21 21:33:17 +0800

    增加

---
+++ src/main/java/com/example/simple/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.simple;
+/**
+ * java基本程序hello，world
+ * @author zhaoliang
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) { 
+		System.out.println("hello,world");
+
+	}
+
+}
---
+++ src/main/java/com/example/simple/Triangle.java
@@ -0,0 +1,14 @@
+package com.example.simple;
+/**
+ * 用打印语句在控制台上打印三角形
+ * @author zhaoliang
+ *
+ */
+public class Triangle {
+	public static void main(String[] args) {
+		System.out.println("   *");
+		System.out.println("  ***");
+		System.out.println(" *****");
+		System.out.println("*******");
+	}
+}
