cbd26e3621d866cb744d9ec11131212df893882e
zhaoliang1 <zhaoliang1@staff.sina.com.cn>
2017-03-28 17:09:07 +0800

    删除没用的类

--- src/main/java/com/example/simple/HelloWorld.java
+++ src/main/java/com/example/simple/HelloWorld.java
@@ -1,4 +1,7 @@
 package com.example.simple;
+
+import java.math.BigDecimal;
+import java.util.*;
 /**
  * java基本程序hello，world
  * @author zhaoliang
@@ -7,8 +10,16 @@ package com.example.simple;
 public class HelloWorld {
 
 	public static void main(String[] args) { 
-		System.out.println("hello,world");
-
+		 Scanner scan  = new Scanner(System.in);
+	        double x = scan.nextInt();
+	        double y = scan.nextInt();
+	        double a = scan.nextInt();
+	        double b = scan.nextInt();
+	        java.text.DecimalFormat myformat=new java.text.DecimalFormat("0.000000000000");
+	        double k = (x * a + y * b) * 1.0 / ( a * a + b * b);
+	        double n = (a * k - x) * 1.0 / b;
+	        System.out.println(myformat.format(k));
+	        System.out.println(myformat.format(n));
 	}
 
 }
