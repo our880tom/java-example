5a9dca73028399c307ba05d450345ca09085852b
zhaoliang1 <zhaoliang1@staff.sina.com.cn>
2017-03-29 15:02:56 +0800

    增加StackOverFlowError

---
+++ src/main/java/com/example/jvm/StackOverFlowDemo.java
@@ -0,0 +1,15 @@
+package com.example.jvm;
+
+public class StackOverFlowDemo {
+	public static void main(String[] args) {
+		method();
+	}
+	
+	public static void method(){
+		for(;;){
+			method();
+		}
+	}
+	
+	//没有设置jvm参数
+}
