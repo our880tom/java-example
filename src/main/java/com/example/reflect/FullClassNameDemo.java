c38e3dff6cb9964a7b0ac77d6be76e95a2faed7c
our880tom <our880tom@163.com>
2017-03-31 15:45:46 +0800

    反射取得类的全名

---
+++ src/main/java/com/example/reflect/FullClassNameDemo.java
@@ -0,0 +1,8 @@
+package com.example.reflect;
+
+public class FullClassNameDemo {
+	public static void main(String[] args) {
+		FullClassNameDemo fullClassNameDemo = new FullClassNameDemo();
+		System.out.println(fullClassNameDemo.getClass().getName());
+	}
+}
